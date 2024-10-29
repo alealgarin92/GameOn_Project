19cfd69a9f2309e5766ed3b228283ddd6b9f3bed
martholito <martholnegri@gmail.com>
2024-10-29 01:27:47 -0300

    Update

---
+++ Assets/Scripts/CargarTarjeta.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class CargarTarjeta : MonoBehaviour
+{
+    [SerializeField] private float pesos;
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        Recarga(other.gameObject);
+
+    }
+
+    public void Recarga(GameObject tarjetasube)
+
+    {
+        var TarjetaSube = tarjetaSube.GetComponent<Tarjeta>();
+        if (tarjetaSube != null)
+        {
+            TarjetaSube.Recarga(pesos * Time.fixedDeltaTime);
+        }
+    }
+
+
+}
+
+
+   
---
+++ Assets/Scripts/Tarjeta.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Tarjeta : MonoBehaviour
+{
+
+    [SerializeField] private float dinero;
+
+    private void Awake()
+    {
+        dinero = 0;
+    }
+
+    public void Recarga(float recargaDinero)
+    {
+        dinero += recargaDinero;
+
+    }
+
+}
