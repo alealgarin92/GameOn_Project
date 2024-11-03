87ff14f84ce2a53cc6c9c0f2f644191b65980240
martholito <martholnegri@gmail.com>
2024-11-03 13:46:49 -0300

    commit area hippie

---
+++ Assets/Scripts/Obstacles/AreaMusicaHippie.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaMusicaHippie : MonoBehaviour
+{ 
+    [SerializeField] private AudioClip musicahippie;
+    [SerializeField] private AudioSource audioSourcemusica;
+
+    private void OnTriggerStay(Collider other)
+    {
+        MusicaChica(other.gameObject);
+    }
+    private void MusicaChica(GameObject target)
+    {
+        MainCharacter mainCharacter = target.GetComponent<MainCharacter>();
+        if (mainCharacter != null)
+        {
+            Playmusichippie();
+        }
+    }
+    public void Playmusichippie()
+    {
+        audioSourcemusica.Play();
+    }
+}
